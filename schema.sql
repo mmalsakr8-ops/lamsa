8d28619973c7b948ee442db9f288d44f23c81dad
mmalsakr8-ops <mmalsakr8@gmail.com>
2026-09-24 00:11:19 +0300

    Delete schema.sql

--- schema.sql
+++
@@ -1,6 +0,0 @@
--- مرجع الجداول الأساسية المستخدمة بواسطة LAMSA.
--- قاعدة البيانات الفعلية يتم إنشاؤها/ترقيتها تلقائيًا من worker.js.
-
--- restaurants includes:
--- menu_enabled INTEGER NOT NULL DEFAULT 1
--- menu_expires_at TEXT
