f4fc12bbb7bb9ca54a22d4b35c243cd8ba34bb37
mmalsakr8-ops <mmalsakr8@gmail.com>
2026-09-24 11:27:44 +0300

    Add files via upload

--- admin.js
+++ admin.js
@@ -2,7 +2,7 @@
 // ADMIN LOGIN
 // =========================
 
-function adminLoginPage() {
+export function adminLoginPage() {
   return `
 <!DOCTYPE html><html lang="ar" dir="rtl"><head>
 <meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1">
